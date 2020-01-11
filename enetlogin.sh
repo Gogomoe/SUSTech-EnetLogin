91694f61454d4104ec0531f65ce727acd3c933cb
Gogo <gogo81745.moe@gmail.com>
2020-01-11 23:04:21 +0800

    local changes

--- enetlogin.sh
+++ enetlogin.sh
@@ -4,21 +4,34 @@ authip="219.134.142.194"
 username="YOUR_USER_NAME_HERE"
 password="YOUR_PASSWORD_HERE"
 
-while [ true ]
+header="User-Agent: Mozilla/5.0"
+
+is_connected=false
+timestamp=$(date "+%s")
+
+while true
 do
-	ret_code=`curl -I -s --connect-timeout 5 http://www.baidu.com -w %{http_code} | tail -n1`
+  ret_code=$(curl -I -s --connect-timeout 5 http://www.baidu.com -w %{http_code} | tail -n1)
 
-	if [ "x$ret_code" != "x200" ] ; then
-		echo "Attempting to log in the enet system"
-		rm -f /tmp/cascookie
+  time=$(date "+[%Y.%m.%d %H:%M:%S]")
+  timestamp_now=$(date "+%s")
 
-		routerip=$(ifconfig | grep -A 1 "eth0.2" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
-		eneturl="http://125.88.59.131:10001/sz/sz112/index.jsp?wlanuserip=$routerip&wlanacip=$authip"
-		execution=$(curl --silent --cookie-jar /tmp/cascookies -k -L "$eneturl" | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
-		
-		curl --silent --output /dev/null --cookie /tmp/cascookies --cookie-jar /tmp/cascookies -H "Content-Type: application/x-www-form-urlencoded" -k -L -X POST "$loginurl" --data "username=$username&password=$password&execution=$execution&_eventId=submit&geolocation="
-	else
-		echo "Connected to Internet, recheck a second later"
-	fi
-	sleep 1s
+  if [ "x$ret_code" != "x200" ] ; then
+    echo "$time Attempting to log in the enet system"
+
+    rm -f /tmp/cascookie
+
+    routerip=$(ifconfig | grep -A 1 "eth0.2" | grep -o "\(inet addr:\).*  Bcast" | grep -o "[0-9\.]*")
+    eneturl="http://enet.10000.gd.cn:10001/sz/sz112/index.jsp?wlanuserip=$routerip&wlanacip=$authip"
+    execution=$(curl --silent --cookie-jar /tmp/cascookies -k -L "$eneturl" -H "$header" | grep -o 'execution.*/><input type' | grep -o '[^"]\{50,\}')
+    echo $execution;
+    curl --silent --output /dev/null --cookie /tmp/cascookies --cookie-jar /tmp/cascookies -H "Content-Type: application/x-www-form-urlencoded" -H "$header" -k -L -X POST "$loginurl" --data "username=$username&password=$password&execution=$execution&_eventId=submit&geolocation="
+  else
+    if [ "$is_connected" == false ] || [ $((timestamp_now - timestamp > 60*60*12)) ]; then
+      timestamp=$timestamp_now
+      echo "$time Connected to Internet, recheck a second later"
+    fi
+    is_connected=true
+  fi
+  sleep 1s
 done
